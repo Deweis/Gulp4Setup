2c9fc60726724afcb3aea17147c058166db1b1bc
Edgar Balsis <edgarbalsis94@gmail.com>
2020-10-24 15:29:32 +0300

    homepage v1.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -27,9 +27,10 @@ let scripts = () => {
         .pipe(browserSync.stream())
 }
 let styles = () => {
-    return src(
-        'app/sass/main.sass'
-    )
+    return src([
+        'app/sass/main.sass',
+        'node_modules/animate.css/animate.min.css'
+    ])
         .pipe(sass())
         .pipe(concat('app.min.css'))
         .pipe(autoPrefixer({
@@ -38,6 +39,17 @@ let styles = () => {
         }))
         .pipe(cleanCss(({ level: { 1: {specialComments: 0 } }, format: 'beautify' })))
         .pipe(dest('app/css/'))
+        .pipe(cleanCss(({ level: { 1: {specialComments: 0 } },})))
+        .pipe(dest('dist/css/'))
+        .pipe(browserSync.stream())
+}
+let fonts = () => {
+    return src(
+        'app/css/fonts.css'
+    )
+        .pipe(concat('fonts.min.css'))
+        .pipe(cleanCss(({ level: { 1: {specialComments: 0 } },})))
+        .pipe(dest('dist/css/'))
         .pipe(browserSync.stream())
 }
 let unMinifyCss = () => {
@@ -71,9 +83,17 @@ let buildCopy = () => {
     ], { base: 'app'})
         .pipe(dest('dist'));
 }
+let html = () => {
+    return src(
+        'app/index.html'
+    )
+        .pipe(concat('index.html'))
+        .pipe(dest('dist'))
+}
 
 function startWatch() {
     watch(['app/**/*.sass'], styles);
+    watch(['app/css/fonts.css'], fonts);
     watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
     watch('app/**/*.html').on('change', browserSync.reload);
     watch(['app/img/src/**/*'], images);
@@ -83,8 +103,11 @@ exports.browserSyncFunc = browserSyncFunc;
 exports.scripts = scripts;
 exports.styles = styles;
 exports.images = images;
+exports.html = html;
 exports.cleanImg = cleanImg;
 exports.unMinifyCss = unMinifyCss;
+exports.fonts = fonts;
+exports.buildCopy = buildCopy;
 exports.build = series(cleanDist, styles, scripts, images, buildCopy);
 
-exports.default = parallel(scripts, styles, images, browserSyncFunc, startWatch);
+exports.default = parallel(scripts, html, styles, fonts, images, browserSyncFunc, startWatch);
